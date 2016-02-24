1cb33f2874a159efb64d6b2f52f018615cbb69d8
Remco Tolsma <info@remcotolsma.nl>
2016-02-24 14:24:05 +0100

    Added Travis CI tests.

---
+++ tests/FormidableFormsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Title: WordPress pay Formidable Forms test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ *
+ * @author Remco Tolsma
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+class Pronamic_WP_Pay_Extensions_FormidableForms_FormidableFormsTest {
+	/**
+	 * Test.
+	 */
+	public function test() {
+		$this->assertTrue( class_exists( 'Pronamic_WP_Pay_Extensions_FormidableForms_FormidableForms' ) );
+	}
+}
