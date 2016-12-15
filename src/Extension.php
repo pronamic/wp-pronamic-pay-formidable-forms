4a227d30a3eb039c6f8779ea519da5c564077e7d
Reüel van der Steege <reuel@pronamic.nl>
2016-12-15 15:23:23 +0100

    Add support for delayed notifications and trigger events.

--- src/Extension.php
+++ src/Extension.php
@@ -19,6 +19,15 @@ class Pronamic_WP_Pay_Extensions_FormidableForms_Extension {
 	const SLUG = 'formidable-forms';
 
 	/**
+	 * Send e-mail now
+	 *
+	 * @var bool
+	 *
+	 * @since unreleased
+	 */
+	static $send_email_now = false;
+
+	/**
 	 * Bootstrap
 	 */
 	public static function bootstrap() {
@@ -31,6 +40,7 @@ class Pronamic_WP_Pay_Extensions_FormidableForms_Extension {
 	public function __construct() {
 		add_action( 'init', array( $this, 'init' ) );
 
+		add_action( 'pronamic_payment_status_update_' . self::SLUG, array( $this, 'update_status' ), 10, 2 );
 		add_filter( 'pronamic_payment_source_text_' . self::SLUG,   array( $this, 'source_text' ), 10, 2 );
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
@@ -42,6 +52,13 @@ class Pronamic_WP_Pay_Extensions_FormidableForms_Extension {
 		// @see https://github.com/wp-premium/formidable/blob/2.0.21/classes/controllers/FrmFormActionsController.php#L299-L308
 		// @see https://github.com/wp-premium/formidable-paypal/blob/3.02/controllers/FrmPaymentsController.php#L28-L29
 		add_action( 'frm_trigger_pronamic_pay_create_action', array( $this, 'create_action' ), 10, 3 );
+
+		// @see https://github.com/wp-premium/formidable-paypal/blob/3.06/controllers/FrmPaymentSettingsController.php#L15-L19
+		add_filter( 'frm_action_triggers', array( $this, 'add_payment_trigger' ) );
+		add_filter( 'frm_email_action_options', array( $this, 'add_trigger_to_action' ) );
+		add_filter( 'frm_twilio_action_options', array( $this, 'add_trigger_to_action' ) );
+		add_filter( 'frm_mailchimp_action_options', array( $this, 'add_trigger_to_action' ) );
+		add_filter( 'frm_register_action_options', array( $this, 'add_payment_trigger_to_register_user_action' ) );
 
 		// Field types
 		$this->field_type_bank_select           = new Pronamic_WP_Pay_Extensions_FormidableForms_BankSelectFieldType();
@@ -78,6 +95,63 @@ class Pronamic_WP_Pay_Extensions_FormidableForms_Extension {
 			wp_enqueue_style( 'pronamic-pay-formidable' );
 		}
 	}
+
+	//////////////////////////////////////////////////
+
+	/**
+	 * Update entry payment status of the specified payment
+	 *
+	 * @param Pronamic_Pay_Payment|string $payment
+	 * @param bool $can_redirect
+	 *
+	 * @since unreleased
+	 */
+	public function update_status( Pronamic_Pay_Payment $payment, $can_redirect = false ) {
+		global $wpdb;
+
+		$entry_id = $payment->get_source_id();
+
+		$entry = FrmEntry::getOne( $entry_id );
+
+		switch ( $payment->status ) {
+			case Pronamic_WP_Pay_Statuses::CANCELLED :
+				FrmFormActionsController::trigger_actions( 'pronamic-pay-cancelled', $entry->form_id, $entry->id );
+				break;
+			case Pronamic_WP_Pay_Statuses::EXPIRED :
+				FrmFormActionsController::trigger_actions( 'pronamic-pay-expired', $entry->form_id, $entry->id );
+				break;
+			case Pronamic_WP_Pay_Statuses::FAILURE :
+				FrmFormActionsController::trigger_actions( 'pronamic-pay-failure', $entry->form_id, $entry->id );
+				break;
+			case Pronamic_WP_Pay_Statuses::SUCCESS :
+				$frm_payment = $wpdb->get_row( $wpdb->prepare( 'SELECT * FROM ' . $wpdb->prefix . 'frm_payments WHERE ( item_id=%d AND (receipt_id = %s OR receipt_id = %s) )', $entry_id, $payment->get_id(), '' ) );
+
+				if ( $frm_payment && '1' !== $frm_payment->completed ) {
+					FrmFormActionsController::trigger_actions( 'pronamic-pay-success', $entry->form_id, $entry->id );
+
+					// Mark the payment as `completed`
+					// @see https://github.com/wp-premium/formidable-paypal/blob/3.06/models/FrmPayment.php#L24-L48
+					$wpdb->update( $wpdb->prefix . 'frm_payments', array( 'completed' => 1 ), array( 'id' => $frm_payment->id ) );
+
+					// Send delayed notifications
+					$form_actions = FrmFormAction::get_action_for_form( $entry->form_id );
+
+					if ( isset( $form_actions[ $frm_payment->action_id ] ) ) {
+						$action = $form_actions[ $frm_payment->action_id ];
+
+						if ( isset( $action->post_content['pronamic_pay_delay_notifications'] ) ) {
+							$this->send_email_now( $entry );
+						}
+					}
+				}
+
+				break;
+			case Pronamic_WP_Pay_Statuses::OPEN :
+				FrmFormActionsController::trigger_actions( 'pronamic-pay-pending', $entry->form_id, $entry->id );
+		}
+	}
+
+	//////////////////////////////////////////////////
 
 	/**
 	 * Source column
@@ -125,6 +199,13 @@ class Pronamic_WP_Pay_Extensions_FormidableForms_Extension {
 		// @see https://github.com/wp-premium/formidable-paypal/blob/3.02/controllers/FrmPaymentsController.php#L268-L269
 		// @see https://github.com/wp-premium/formidable/blob/2.0.21/classes/models/FrmEntry.php#L698-L711
 		add_action( 'frm_after_create_entry', array( $this, 'redirect_for_payment' ), 50, 2 );
+
+		// Delay notifications
+		if ( ! self::$send_email_now && 'on' === $action->post_content['pronamic_pay_delay_notifications'] ) {
+			remove_action( 'frm_trigger_email_action', 'FrmNotification::trigger_email', 10, 3 );
+			add_filter( 'frm_to_email', '__return_empty_array', 20 );
+			add_filter( 'frm_send_new_user_notification', array( __CLASS__, 'stop_registration_email' ), 10, 3 );
+		}
 	}
 
 	/**
@@ -133,6 +214,8 @@ class Pronamic_WP_Pay_Extensions_FormidableForms_Extension {
 	 * @see https://github.com/wp-premium/formidable-paypal/blob/3.02/controllers/FrmPaymentsController.php#L274-L311
 	 */
 	public function redirect_for_payment( $entry_id, $form_id ) {
+		global $wpdb;
+
 		$config_id = get_option( 'pronamic_pay_config_id' );
 
 		$gateway = Pronamic_WP_Pay_Plugin::get_gateway( $config_id );
@@ -142,6 +225,22 @@ class Pronamic_WP_Pay_Extensions_FormidableForms_Extension {
 
 			$payment = Pronamic_WP_Pay_Plugin::start( $config_id, $gateway, $data, Pronamic_WP_Pay_PaymentMethods::IDEAL );
 
+			// Add new Formidable payment to be able to check `completed` on status update
+			// @see https://github.com/wp-premium/formidable-paypal/blob/3.06/models/FrmPayment.php#L5-L22
+			$frm_payment = array(
+				'receipt_id'  => $payment->get_id(),
+				'item_id'     => $entry_id,
+				'action_id'   => $this->action->ID,
+				'amount'      => (float) $data->get_amount(),
+				'completed'   => 0,
+				'begin_date'  => current_time( 'mysql', true ),
+				'expire_date' => '0000-00-00',
+				'paysys'      => Pronamic_WP_Pay_Extensions_FormidableForms_PaymentAction::SLUG,
+				'created_at'  => current_time( 'mysql', 1 ),
+			);
+
+			$wpdb->insert( $wpdb->prefix . 'frm_payments', $frm_payment );
+
 			$error = $gateway->get_error();
 
 			if ( ! is_wp_error( $error ) ) {
@@ -149,5 +248,109 @@ class Pronamic_WP_Pay_Extensions_FormidableForms_Extension {
 				$gateway->redirect( $payment );
 			}
 		}
+	}
+
+	/**
+	 * Stop registration email.
+	 *
+	 * @param $send_it
+	 * @param $form
+	 * @param $entry_id
+	 *
+	 * @return bool
+	 *
+	 * @since unreleased
+	 */
+	public static function stop_registration_email( $send_it, $form, $entry_id ) {
+		if ( ! is_callable( 'FrmRegAppController::send_paid_user_notification' ) ) {
+			// Don't stop the registration email unless the function
+			// from the Formidable Registration Add-On exists to send it later
+
+			return $send_it;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Send email now.
+	 *
+	 * @param $entry
+	 *
+	 * @since unreleased
+	 */
+	public static function send_email_now( $entry ) {
+		self::$send_email_now = true;
+
+		// Trigger email action
+		if ( is_callable( 'FrmFormActionsController::trigger_actions' ) ) {
+			// 2.0
+			FrmFormActionsController::trigger_actions( 'create', $entry->form_id, $entry->id, 'email' );
+		} elseif ( is_callable( 'FrmProNotification::entry_created' ) ) {
+			// < 2.0
+			FrmProNotification::entry_created( $entry->id, $entry->form_id );
+		}
+
+		// Trigger registration email
+		if ( is_callable( 'FrmRegNotification::send_paid_user_notification' ) ) {
+			FrmRegNotification::send_paid_user_notification( $entry );
+		} elseif ( is_callable( 'FrmRegAppController::send_paid_user_notification' ) ) {
+			FrmRegAppController::send_paid_user_notification( $entry );
+		}
+	}
+
+	/**
+	 * Add payment trigger.
+	 *
+	 * @param $triggers
+	 *
+	 * @return array
+	 *
+	 * @since unreleased
+	 */
+	public static function add_payment_trigger( $triggers ) {
+		$triggers['pronamic-pay-pending']   = __( 'Pronamic payment pending', 'pronamic_ideal' );
+		$triggers['pronamic-pay-success']   = __( 'Pronamic payment success', 'pronamic_ideal' );
+		$triggers['pronamic-pay-cancelled'] = __( 'Pronamic payment cancelled', 'pronamic_ideal' );
+		$triggers['pronamic-pay-expired']   = __( 'Pronamic payment expired', 'pronamic_ideal' );
+		$triggers['pronamic-pay-failed']    = __( 'Pronamic payment failed', 'pronamic_ideal' );
+
+		return $triggers;
+	}
+
+	/**
+	 * Add trigger to action.
+	 *
+	 * @param $options
+	 *
+	 * @return array
+	 *
+	 * @since unreleased
+	 */
+	public static function add_trigger_to_action( $options ) {
+		$options['event'][] = 'pronamic-pay-pending';
+		$options['event'][] = 'pronamic-pay-success';
+		$options['event'][] = 'pronamic-pay-cancelled';
+		$options['event'][] = 'pronamic-pay-expired';
+		$options['event'][] = 'pronamic-pay-failed';
+
+		return $options;
+	}
+
+	/**
+	 * Add payment trigger to registration 2.0+
+	 *
+	 * @param array $options
+	 *
+	 * @return array
+	 *
+	 * @since unreleased
+	 */
+	public static function add_payment_trigger_to_register_user_action( $options ) {
+		if ( is_callable( 'FrmRegUserController::register_user' ) ) {
+			$options['event'][] = 'pronamic-pay-success';
+		}
+
+		return $options;
 	}
 }
