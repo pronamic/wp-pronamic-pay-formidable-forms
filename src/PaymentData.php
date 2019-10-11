98c7a536a6e029908156bcf289f93078619675ce
Reüel van der Steege <reuel@akwaabamultimedia.nl>
2019-10-11 20:32:07 +0200

    No longer use deprecated `addItem()` method.

--- src/PaymentData.php
+++ src/PaymentData.php
@@ -149,7 +149,7 @@ class PaymentData extends Pay_PaymentData {
 		$item->set_price( $this->get_amount_from_field() );
 		$item->set_quantity( 1 );
 
-		$items->addItem( $item );
+		$items->add_item( $item );
 
 		return $items;
 	}
