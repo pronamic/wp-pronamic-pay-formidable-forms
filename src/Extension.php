6b623784ec72b94ed91578e9ae4288dc2db8c994
Reüel van der Steege <reuel@pronamic.nl>
2019-12-22 21:27:37 +0100

    Update version 2.0.4.

--- src/Extension.php
+++ src/Extension.php
@@ -21,7 +21,7 @@ use Pronamic\WordPress\Pay\Plugin;
  * Company: Pronamic
  *
  * @author  Remco Tolsma
- * @version 2.0.0
+ * @version 2.0.4
  * @since   1.0.0
  */
 class Extension {
--- src/PaymentMethodSelectFieldType.php
+++ src/PaymentMethodSelectFieldType.php
@@ -17,7 +17,7 @@ use Pronamic\WordPress\Pay\Plugin;
  * Company: Pronamic
  *
  * @author  Remco Tolsma
- * @version 2.0.0
+ * @version 2.0.4
  * @since   1.0.0
  */
 class PaymentMethodSelectFieldType {
